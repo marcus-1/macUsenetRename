c5d67f246e45812a13254110355bdef23920a9d9
Marcus <Marcus@Marcus-iMac.local>
2015-05-31 17:19:07 +0200

    Initial Commit

---
+++ macUsenetRename/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  macUsenetRename
+//
+//  Created by Marcus on 31.05.15.
+//  Copyright (c) 2015 Marcus. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
